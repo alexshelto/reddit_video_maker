5684514f18e077efa39f3d0b64ed22d564fce30a
Alex Shelton <alexandershelton@icloud.com>
2021-04-04 23:48:04 -0400

    cant find a damn male voice

--- src/run.py
+++ src/run.py
@@ -10,17 +10,19 @@ from RedditScrape import RedditScrape
 
 
 def main() -> int: 
+    # Creating required command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('url', help='link of reddit post is required')
     parser.add_argument('n_entries', help='specify the number of replies to the post you want')
+    # Parse the command line arguments
     args = parser.parse_args()
-
 
     '''
     this is where we will create a class instance of Reddit scraper and video maker
     Reddit class will return or create mp3 clips that will be used by the 
     video class afer 
     '''
+
     print("you want to make a video with the link: ", args.url)
 
     # Creating the reddit scraper class
@@ -28,7 +30,6 @@ def main() -> int:
 
     # Calling a function on the reddit scraper 
     reddit_scraper.scrape_post()
-
 
 
     return 0
