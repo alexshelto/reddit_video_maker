4e548ef8cf00edbca7441bd09fd0a1bf7092104e
Alex Shelton <39957709+alexshelto@users.noreply.github.com>
2021-06-16 21:01:58 -0700

    Create config.py
    demo config file

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# Obtain Praw credentails and add your client id and secret below in their respected fields
+# For help try visiting: https://www.jcchouinard.com/get-reddit-api-credentials-with-praw/
+#              and: reddit.com/prefs/apps
+
+PRAW_CONFIG = {
+    'client_id': '',
+    'client_secret': '',
+    'user_agent': 'user-agent'
+    }
