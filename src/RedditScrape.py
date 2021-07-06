516620814908b395666c5a9c0a570ae2813cd4a6
tpjwm <dimitarpendurkov@gmail.com>
2021-07-06 11:22:23 -0500

    Changed config to use environment variables and some reformatting

--- src/RedditScrape.py
+++ src/RedditScrape.py
@@ -7,25 +7,21 @@
 #
 
 
-#importing the config.py file to connect to PRAW
-import config
-#including the reddit api wrapper
-import praw
-
+# importing the config.py file to connect to PRAW
+from config import reddit_api
 
 # Pre process can let us exchange words. ie: exchanging curse words for others
 from gtts.tokenizer import pre_processors
 import gtts.tokenizer.symbols
 
 import sys
+
 sys.path.append("../")
-
-
 
 # Words that we will exchange for others in text scraping
 # To ensure no terrible words are used in the video
 gtts.tokenizer.symbols.SUB_PAIRS.append(
-        ('fuck', '*uck')
+    ('fuck', '*uck')
 )
 
 
@@ -38,9 +34,8 @@ class RedditScrape:
 
         self.url = url
         self.num_replies = num_replies
-        self.path = '../audio/' # Creating a directory to hold the audio in
-         
-    
+        self.path = '../audio/'  # Creating a directory to hold the audio in
+
     def scrape_post(self):
         '''Takes the link passed into the class constructor
         to scrape the reddit post for the title and the top comments
@@ -48,44 +43,35 @@ class RedditScrape:
         a text to speech mp3 files and writes them to an mp3'''
 
         # Creating an instance of reddit api
-        reddit = praw.Reddit(
-            client_id=config.PRAW_CONFIG['client_id'],
-            client_secret=config.PRAW_CONFIG['client_secret'],
-            user_agent=config.PRAW_CONFIG['user_agent'],
-        )
+        reddit = reddit_api()
 
-        text_used = [] #Creating list filled with strings of the title and all comment text
-        authors = [] #Creating a list of authors from the post strings
+        text_used = []  # Creating list filled with strings of the title and all comment text
+        authors = []  # Creating a list of authors from the post strings
 
-        submission = reddit.submission(url=self.url) #Getting submission post
-        submission.comment_sort = 'top' # Sorting the comments on the post to top voted
-        submission.comments.replace_more(limit=0) #removing weird 'more' comments
+        submission = reddit.submission(url=self.url)  # Getting submission post
+        submission.comment_sort = 'top'  # Sorting the comments on the post to top voted
+        submission.comments.replace_more(limit=0)  # removing weird 'more' comments
 
         # Creating a list of the top n replies, n=num_replies. an argument to the class
-        comments = submission.comments.list()[0:self.num_replies] #Change this to remove instance of getting "[deleted]" comments sometimes
+        comments = submission.comments.list()[
+                   0:self.num_replies]  # Change this to remove instance of getting "[deleted]" comments sometimes
 
         # adding post author and replies authors
         authors.append(submission.author.name)
         for comment in comments:
-            try: 
+            try:
                 authors.append(comment.author.name)
             except AttributeError:
                 authors.append('deleted')
 
-
-
         clean_title = pre_processors.word_sub(submission.title)
         text_used.append(clean_title)
-        
 
         for i in range(0, len(comments)):
             # Push cleaned string into text_used
             data = reddit.comment(comments[i])
             clean_str = pre_processors.word_sub(data.body)
-            text_used.append(clean_str)#.encode('utf-8', 'replace'))
-
+            text_used.append(clean_str)  # .encode('utf-8', 'replace'))
 
         # Returns text used: [title & replies], and [authors]
         return text_used, authors
-
-
--- src/config.py
+++ src/config.py
@@ -1,9 +1,14 @@
-# Obtain Praw credentails and add your client id and secret below in their respected fields
+# Obtain Praw credentials and add your client id, secret, and user agent to environment variables
 # For help try visiting: https://www.jcchouinard.com/get-reddit-api-credentials-with-praw/
 #              and: reddit.com/prefs/apps
+import os
+import praw
 
-PRAW_CONFIG = {
-    'client_id': '',
-    'client_secret': '',
-    'user_agent': 'user-agent'
-    }
+
+def reddit_api():
+    reddit = praw.Reddit(
+        client_id=os.getenv("CLIENT_ID"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+        user_agent=os.getenv("USERNAME"))
+
+    return reddit
